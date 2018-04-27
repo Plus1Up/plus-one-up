de1322466571e7520593f309fe2b3736ee7c6749
Diana <diana.mielniczek@gmail.com>
2018-04-27 18:08:51 +0200

    [Diets] dodawanie i pobieranie planu diety w .pdf

--- app/views/Coach/Clients/ClientInfo/Diet/index.js
+++ app/views/Coach/Clients/ClientInfo/Diet/index.js
@@ -1,17 +1,65 @@
-import React, {Component} from "react";
-import {Table, TableBody, TableHeader, TableHeaderColumn, TableRow, TableRowColumn, DropDownMenu, MenuItem, RaisedButton} from "material-ui";
-import {Link} from "react-router-dom";
-import "./styles.css";
+import React, {Component} from 'react';
+import {Table, TableBody, TableHeader, TableHeaderColumn, TableRow, TableRowColumn, DropDownMenu, MenuItem, RaisedButton} from 'material-ui';
+import {Link} from 'react-router-dom';
+import {FileSaver} from 'file-saver';
+import { clients, diets } from "services/api";
+import './styles.css';
 
-import Page from "components/Page";
-import dietImg from "assets/images/diet.png";
+import Page from 'components/Page';
 
 class Diet extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
+      isHidden: true,
+      selectedFile: null
     };
+  }
+
+  get clientId() {
+    return this.props.id;
+  }
+
+  toggleHidden () {
+    this.setState({
+      isHidden: !this.state.isHidden
+    })
+  }
+
+  fileSelectedHandler = event => {
+    this.setState({
+      selectedFile: event.target.files[0]
+    })
+  }
+
+  fileUploadHandler = () => {
+    const fd = new FormData();
+    fd.append('diet_plan', this.state.selectedFile);
+    const config = {
+      headers: {
+        'content-type': 'multipart/form-data'
+      }
+    }
+    clients
+      .put(this.clientId, fd, config)
+      .then(response => {
+        console.log(response);
+      });
+  }
+
+  fileDownloadHandler() {
+    const FileSaver = require('file-saver');
+    const axios = require('axios');
+    diets
+     .get(10, "dieta_cud.pdf")
+     .then(response => {
+        console.log(response);
+        const blob = new Blob([response.data], {
+          type: 'application/pdf',
+        });
+        FileSaver.saveAs(blob, Math.random().pdf);
+    });
   }
 
   componentDidMount() {
@@ -20,14 +68,22 @@ class Diet extends Component {
   render() {
     return (
       <div>
-        <div className="container">
-          <div className="half-container">
-            <img src={dietImg} />
+        <div className='container'>
+          <div className='half-container'>
+            <img src='../../../assets/images/diet.png' />
           </div>
-          <div className="half-container">
-          <RaisedButton label="Pobierz plan" className="plan-btn"></RaisedButton>
-          <RaisedButton label="Usuń plan" className="plan-btn"></RaisedButton>
-          <RaisedButton label="Nowy plan" className="plan-btn"></RaisedButton>
+          <div className='half-container'>
+          <RaisedButton label='Pobierz plan' className='plan-btn' onClick={this.fileDownloadHandler}></RaisedButton>
+          
+          <RaisedButton label='Nowy plan' className='plan-btn' onClick={this.toggleHidden.bind(this)} ></RaisedButton>
+            {!this.state.isHidden && <div className='hidden-container'>
+              <input 
+              type="file" 
+              onChange={this.fileSelectedHandler} />
+              <RaisedButton label='Dodaj' className='plan-btn' onClick={this.fileUploadHandler}></RaisedButton>
+            </div>}
+
+          <RaisedButton label='Usuń plan' className='plan-btn'></RaisedButton>
           </div>
         </div>
       </div>
